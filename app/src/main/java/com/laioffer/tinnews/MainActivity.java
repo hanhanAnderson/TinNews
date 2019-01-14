d393d826ccac54c1b3c2bca86b6da46ad02c5bbc
HanhanAnderson <Hanhan@gwu.edu>
2019-01-14 12:59:38 -0500

    Add view Pager

--- app/src/main/java/com/laioffer/tinnews/MainActivity.java
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -1,6 +1,8 @@
 package com.laioffer.tinnews;
 
 import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +11,18 @@ import android.widget.TextView;
 import com.laioffer.tinnews.common.TinBasicActivity;
 
 public class MainActivity extends TinBasicActivity {
+    private ViewPager viewPager;
+    private BottomNavigationView bottomBar;
+    private TinFragmentPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new TinFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        viewPager.setOffscreenPageLimit(TinFragmentPagerAdapter.FRAGMENT_NUMBER);
+
 //        setContentView(R.layout.activity_main);
 //        android1 10-1
 
