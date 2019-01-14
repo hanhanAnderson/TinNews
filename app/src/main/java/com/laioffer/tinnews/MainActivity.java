24290cd34e95deb8a1caeaa6b55d8362f28f32d3
HanhanAnderson <Hanhan@gwu.edu>
2019-01-13 23:22:04 -0500

    ReStructure, Add icon

--- app/src/main/java/com/laioffer/tinnews/MainActivity.java
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.laioffer.tinnews.common.TinBasicActivity;
+
+public class MainActivity extends TinBasicActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 //        android1 10-1
         TextView view = findViewById(R.id.text);
         view.setOnClickListener(new View.OnClickListener() {
@@ -19,8 +21,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void showSnackBar(String message) {
+
     }
 }
 
